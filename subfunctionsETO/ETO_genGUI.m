9f5679d5eab104c241961f9eb5888742e144c926
JorritMontijn <jorritmontijn@gmail.com>
2022-03-07 15:20:33 +0100

    Revert "20210920"
    This reverts commit 26f63281faa53568c08ff6f25c434430d3fa3dd2.

--- subfunctionsETO/ETO_genGUI.m
+++ subfunctionsETO/ETO_genGUI.m
@@ -177,6 +177,8 @@ function [sFigETO,sETO] = ETO_genGUI(varargin)
 						indReady(intFileIdx) = true;
 					elseif exist(fullfile(sETO.sFiles(intFile).sPupil.folder,[strMiniFile strUseExt]),'file')
 						indReady(intFileIdx) = true;
+					else
+						indReady(intFileIdx) = false;
 					end
 				end
 			catch
